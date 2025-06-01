22d5717058b0c45cba918d43551462b715826924
Machireddy123 <chaithanyamachireddy201@gmail.com>
2025-05-31 19:09:43 -0500

    Updated test_operations.py file due to errors in worflow

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,9 +3,9 @@ from src.math_operations import add, sub
 def test_add():
     assert add(2,3) == 5 # assert func gives the output as true if 2,3 output is equal 5 else it gives false
     assert add(-1,1) == 0
-    assert add(100, 500) == 300
+    assert add(100, 500) == 600
 
 def test_sub():
     assert sub(12,10) == 2
     assert sub(20,30) == -10
-    assert sub(50,70) == 20
+    assert sub(50,70) == -20
