2c97dd3161c448312abd27c86382d38daa6a08fe
Machireddy123 <chaithanyamachireddy201@gmail.com>
2025-05-31 19:04:41 -0500

    Updated changes in test_operation.py file

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,7 +3,7 @@ from src.math_operations import add, sub
 def test_add():
     assert add(2,3) == 5 # assert func gives the output as true if 2,3 output is equal 5 else it gives false
     assert add(-1,1) == 0
-    assert add(100 + 500) == 300
+    assert add(100, 500) == 300
 
 def test_sub():
     assert sub(12,10) == 2
